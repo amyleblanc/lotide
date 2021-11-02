1f25c6331939301306989ef559156001ca6fac0c
Amy <amymccarthy2014@gmail.com>
2021-11-02 04:34:02 +0000

    added assertArraysEqual tests

--- takeUntil.js
+++ takeUntil.js
@@ -12,31 +12,27 @@ const takeUntil = function(array, callback) {
 
 
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-const results1 = takeUntil(data1, x => x < 0);
+const results1 = takeUntil(data1, x => x < 0); // [ 1, 2, 5, 7, 2 ]
 console.log(results1);
 
 console.log('---');
 
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-const results2 = takeUntil(data2, x => x === ',');
+const results2 = takeUntil(data2, x => x === ','); // [ 'I\'ve', 'been', 'to', 'Hollywood' ]
 console.log(results2);
 
-///////////////////////////////////
+const assertArraysEqual = (first, second) => { // compare arrays using the eqArrays function
+  const eqArrays = (first, second) => {
+    let firstString = first.toString();
+    let secondString = second.toString();
+    if (firstString === secondString) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+  console.log(`Are these two arrays the same? Answer: ${eqArrays(first, second)}`);
+};
 
-// const sliceAtComma = function(array) {
-//   let index = array.indexOf(',');
-//   return array.slice(0, index);
-// };
-
-
-
-// const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-// console.log(takeUntil(data1, x => x < 0));
-
-
-// // const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-
-
-// console.log(takeUntil(data1, sliceAtZero));
-// console.log('---');
-// console.log(takeUntil(data2, sliceAtComma));
+assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ]); // True
+assertArraysEqual(results2, [ 'I\'ve', 'been', 'to', 'Hollywood' ]); // True
