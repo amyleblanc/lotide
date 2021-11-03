712572b00026ff78abb27e10da29df7a5720efc9
Amy <amymccarthy2014@gmail.com>
2021-11-03 02:31:19 +0000

    fix code, add assertEqual test

--- findKey.js
+++ findKey.js
@@ -2,18 +2,36 @@ const assertEqual = function(actual, expected) {
   actual === expected ? console.log(`🥳 Assertion Passed: ${actual} === ${expected}`) : console.log("🤬 Assertion Failed: " + actual + " !== " + expected);  // TERNARY OPERATOR
 };
 
-const findKey = function(object, calback) {
-  
-}
-
-// REFER TO:
-
-const findKeyByValue = function(object, test) {
-  for (const key of Object.keys(object)) {
-    const value = object[key];
-    if (value === test) {
-      return key;
+const findKey = function(object, callback) {
+  let truthyValues = []; // create empty array for truthy results
+  for (let key in object) { // loop over object
+    if (callback(object[key])) { // apply callback to object values
+      truthyValues.push(key);  // push truthy results to empty array
     }
   }
-  return undefined;
+  if (truthyValues < 1) {  // if no truthy results, return undefined
+    return undefined;
+  } else {
+    return truthyValues[0]; // if array has truthy results, return first instance
+  }
 };
+
+findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), 'noma');
+
+    
