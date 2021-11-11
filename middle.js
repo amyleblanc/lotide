5f599a8554c21ad1f17f9348906016930aa4010d
Amy McCarthy <amymccarthy2014@gmail.com>
2021-11-11 23:54:26 +0000

    refactor code

--- middle.js
+++ middle.js
@@ -1,26 +1,3 @@
-const assertArraysEqual = (first, second) => { // compare arrays using the eqArrays function
-  const eqArrays = (first, second) => {
-    let firstString = first.toString();
-    let secondString = second.toString();
-    if (firstString === secondString) {
-      return true;
-    } else {
-      return false;
-    }
-  };
-  console.log(`Are these two arrays the same? Answer: ${eqArrays(first, second)}`);
-};
-
-const eqArrays = (first, second) => {
-  let firstString = first.toString();
-  let secondString = second.toString();
-  if (firstString === secondString) {
-    return true;
-  } else {
-    return false;
-  }
-};
-
 const middle = (array) => {
   let midIndex = Math.floor((array.length - 1) / 2);
   let midArray = [];
@@ -38,18 +15,4 @@ const middle = (array) => {
   }
 };
 
-// TEST CODE:
-
-console.log(middle([1])); // => []
-console.log(middle([1, 2])); // => []
-console.log(middle([1, 2, 3])); // => [2]
-console.log(middle([1, 2, 3, 4, 5])); // => [3]
-console.log(middle([1, 2, 3, 4])); // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
-
-console.log(assertArraysEqual(middle([1]), []));
-console.log(assertArraysEqual(middle([1, 2]), []));
-console.log(assertArraysEqual(middle([1, 2, 3]), [2]));
-console.log(assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]));
-console.log(assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]));
-console.log(assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]));
+module.exports = middle;
