15886357589cfdc6799f26e523d6e632be37ab40
Amy McCarthy <amymccarthy2014@gmail.com>
2021-11-11 03:35:26 +0000

    refactor code

--- head.js
+++ head.js
@@ -1,19 +1,7 @@
-const assertEqual = function(actual, expected) {
-  /*if (actual === expected) {
-    console.log(`🥳 Assertion Passed: ${actual} === ${expected}`); // STRING INTERPOLATION VERSION
-  } else {
-    console.log("🤬 Assertion Failed: " + actual + " !== " + expected);  // ORIGINAL CODE VERSION
-  }
-  */
-  actual === expected ? console.log(`🥳 Assertion Passed: ${actual} === ${expected}`) : console.log("🤬 Assertion Failed: " + actual + " !== " + expected);  // TERNARY OPERATOR
-};
-
 const head = function(array) {
   for (let i = 0; i <= array.length; i++) {
     return array[0];
   }
 };
 
-// TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
