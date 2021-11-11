fa69cd4a90ac32824e29664bce19dbe871f814c7
Amy McCarthy <amymccarthy2014@gmail.com>
2021-11-11 23:37:57 +0000

    refactor code

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,22 +1,7 @@
-// const eqArrays = require('./eqArrays'); // Mentor Elbert Bae showed me a more efficient way to reuse my eqArrays function
+const eqArrays = require('./eqArrays');
 
-// const assertArraysEqual = (first, second) => { // compare arrays using the eqArrays file
-//   console.log(`Are these two arrays the same? Answer: ${eqArrays(first, second)}`) ;
-// };
-
-const assertArraysEqual = (first, second) => { // compare arrays using the eqArrays function
-  const eqArrays = (first, second) => {
-    let firstString = first.toString();
-    let secondString = second.toString();
-    if (firstString === secondString) {
-      return true;
-    } else {
-      return false;
-    }
-  };
+const assertArraysEqual = (first, second) => { // compare arrays using the eqArrays function and print the result
   console.log(`Are these two arrays the same? Answer: ${eqArrays(first, second)}`);
 };
 
-// TEST CODE:
-// assertArraysEqual([1, 2, 3], [1, 2, 3]);
-// assertArraysEqual([1, 2, 3], [1, 2, 2]);
+module.exports = assertArraysEqual;
