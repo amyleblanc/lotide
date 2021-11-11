e469af562431902d3ab8be66961e4e031d89a457
Amy McCarthy <amymccarthy2014@gmail.com>
2021-11-11 03:34:57 +0000

    refactor code

--- assertEqual.js
+++ assertEqual.js
@@ -1,10 +1,5 @@
-// FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   actual === expected ? console.log(`🥳 ✅ Assertion Passed: ${actual} === ${expected}`) : console.log("🤬 ⛔️ Assertion Failed: " + actual + " !== " + expected);  // TERNARY OPERATOR
 };
 
-// TEST CODE
-// assertEqual("Lighthouse Labs", "Bootcamp");
-// assertEqual(1, 1);
-// assertEqual("Bootcamp", "Bootcamp");
-// assertEqual(2, 1);
+module.exports = assertEqual;
