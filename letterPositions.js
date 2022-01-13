05f1dd190a1156f1264d6439f24da310137832dc
Amy McCarthy <amymccarthy2014@gmail.com>
2022-01-13 04:24:00 +0000

    change for loop type and fix bug that changed index num to string; remove test code

--- letterPositions.js
+++ letterPositions.js
@@ -1,23 +1,13 @@
-// const _ = require('./index'); // **NB** UNCOMMENT WHEN USING TEST CODE
-
 const letterPositions = function(sentence) {
   const results = {}; // create empty object to contain eventual result
-  for (let letter in sentence) { // loop through string indices
-    if (results[sentence[letter]]) { // check if the letter element exists as a key within "results" object
-      results[sentence[letter]].push(letter); // if it does exist, push index to the value
+  for (let i = 0; i < sentence.length; i++) { // loop through string indices
+    if (results[sentence[i]]) { // check if the letter element exists as a key within "results" object
+      results[sentence[i]].push(i); // if it does exist, push index to the value
     } else {
-      results[sentence[letter]] = [letter]; // if it does not exist, create a new key: value pair
+      results[sentence[i]] = [i]; // if it does not exist, create a new key: value pair
     }
   }
   return results;
 };
 
 module.exports = letterPositions;
-
-// TEST CODE:
-
-// console.log(letterPositions('hello'));
-// console.log(letterPositions('successful'));
-
-// _.assertArraysEqual(letterPositions('hello')['e'], [1]);
-// _.assertArraysEqual(letterPositions('successful')['s'], [0, 5, 6]);
