4adfe0a43984422ec33fda2b6299e0c63908b871
Amy McCarthy <amymccarthy2014@gmail.com>
2022-01-13 04:18:09 +0000

    remove comments

--- countLetters.js
+++ countLetters.js
@@ -1,5 +1,3 @@
-// const _ = require('./index'); // **NB** UNCOMMENT WHEN USING TEST CODE
-
 const countLetters = function(sentence) {
   let numOfLetters = {};
   for (const letter of sentence) {
@@ -15,23 +13,3 @@ const countLetters = function(sentence) {
 };
 
 module.exports = countLetters;
-
-// TEST CODE:
-
-// console.log(countLetters('hello')); // { h: 1, e: 1, l: 2, o: 1 }
-// console.log(countLetters('Hi, how are you?')); // { H: 1, i: 1, h: 1, o: 2, w: 1, a: 1, r: 1, e: 1, y: 1, u: 1 }
-// console.log(countLetters('I am quite well, thank you! How are you?'));
-
-
-// const result1 = countLetters('hello');
-
-// _.assertEqual((result1['l']), 2); // { l: 2 }
-// _.assertEqual((result1['h']), 1); // { h: 1 }
-
-// const result2 = countLetters('Hi, how are you?');
-
-// _.assertEqual((result2['o']), 2); // { o: 2 }
-
-// const result3 = countLetters('I am quite well, thank you! How are you?');
-
-// _.assertEqual((result3['e']), 3); // { e: 3 }
